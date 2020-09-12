828a834cc7d183ffef1d475f10e1b3ded79ce8b7
Hoang Pham <pham.hoang1591@gmail.com>
2020-09-12 19:26:49 +0300

    refactor scripts.js

--- scripts.js
+++ scripts.js
@@ -1,62 +1,74 @@
-let birds = [
-  "Crow",
-  "Peacock",
-  "Dove",
-  "Sparrow",
-  "Goose",
-  "Ostrich",
-  "Pigeon",
-  "Turkey",
-  "Hawk",
-  "Raven",
-];
+function App() {
+  let birds = [
+    "Crow",
+    "Peacock",
+    "Dove",
+    "Sparrow",
+    "Goose",
+    "Ostrich",
+    "Pigeon",
+    "Turkey",
+    "Hawk",
+    "Raven",
+  ];
 
-const instructions = {
-  pressFirst: "Press the key as the first letter of the bird's name",
-  pressLast: "Press the key as the last letter of the bird's name",
-  pressAny: "Press any key to restart",
-};
+  const instructions = {
+    pressFirst: "Press the key as the first letter of the bird's name",
+    pressLast: "Press the key as the last letter of the bird's name",
+    pressAny: "Press any key to restart",
+  };
 
-let firstLetter = true;
-let index = 0;
+  let position = {
+    firstLetter: true,
+    index: 0,
+  };
 
-document.getElementById("name").innerText = birds[index];
-document.addEventListener("keydown", show);
+  document.getElementById("name").innerText = birds[position.index];
+  document.addEventListener("keydown", (e) => {
+    show(e, position, instructions, birds);
+  });
+}
 
-function show(e) {
+function show(e, position, instructions, birds) {
   let name = document.getElementById("name").innerText;
   if (name) {
-    character = firstLetter
+    character = position.firstLetter
       ? name[0].toLowerCase()
       : name[name.length - 1].toLowerCase();
     if (e.key == character) {
-      name = firstLetter ? name.slice(1) : name.slice(0, -1);
+      name = position.firstLetter ? name.slice(1) : name.slice(0, -1);
       if (!name) {
-        next(undefined, true);
+        next(undefined, position, instructions, birds);
       } else {
-        firstLetter ? next(name, false) : next(name, true);
+        next(name, position, instructions, birds);
       }
     }
   } else {
-    next(undefined, true);
+    next(undefined, position, instructions, birds);
   }
 }
 
-function next(name, first) {
+function next(name, position, instructions, birds) {
+  position.firstLetter = name ? !position.firstLetter : true;
+  nextUlti(name, position, instructions, birds);
+}
+
+function nextUlti(name, position, instructions, birds) {
   if (!name) {
-    index++;
-    document.getElementById("name").innerText = birds[index]
-      ? birds[index]
+    position.index++;
+    document.getElementById("name").innerText = birds[position.index]
+      ? birds[position.index]
       : "";
-    document.getElementById("press").innerText = birds[index]
+    document.getElementById("press").innerText = birds[position.index]
       ? instructions["pressFirst"]
       : instructions["pressAny"];
-    index = birds[index] ? index : -1;
+    position.index = birds[position.index] ? position.index : -1;
   } else {
     document.getElementById("name").innerText = name;
-    document.getElementById("press").innerText = first
+    document.getElementById("press").innerText = position.firstLetter
       ? instructions["pressFirst"]
       : instructions["pressLast"];
   }
-  firstLetter = first;
 }
+
+App();
